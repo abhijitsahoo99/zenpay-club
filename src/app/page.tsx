4400928c2090314b66140f387efa2052494e3414
abhijitsahoo99 <abhijitsahooas.01@gmail.com>
2024-08-09 19:50:05 +0530

    add: initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import styles from "@/styles/Home.module.scss";
+
+export default function Home() {
+  return (
+    <div className={styles.landing}>
+      <h1>zenpay</h1>
+      <p>all in one payment aggregator</p>
+      <div>
+        <button>get started</button>
+      </div>
+    </div>
+  );
+}
