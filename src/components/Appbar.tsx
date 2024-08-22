265e22d739913e9ba853368937e0425e65f1f09b
abhijitsahoo99 <abhijitsahooas.01@gmail.com>
2024-08-23 01:53:49 +0530

    fix: connect wallet btn

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -1,12 +1,13 @@
 import styles from "@/styles/Appbar.module.scss";
+import ShinyButton from "@/components/magicui/shiny-button";
 import React from "react";
 
 function Appbar() {
   return (
     <div className={styles.navbar}>
       <div className={styles.logo}>zenpay.club</div>
-      <div className={styles.walletbtn}>
-        <button>connect wallet</button>
+      <div>
+        <ShinyButton text="connect wallet" className="border-2 rounded-xl" />
       </div>
     </div>
   );
